d8b9e456ba9422fbf2cbd17a8a3341d61914afff
unknown <ivanmist90@gmail.com>
2021-11-07 21:44:29 +0100

    Things are going wind in pope a toda vela

--- src/app/components/custom/music/music.component.ts
+++ src/app/components/custom/music/music.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Player } from '@vime/angular';
 
 @Component({
@@ -11,16 +11,38 @@ export class MusicComponent implements OnInit {
   // Obtain a ref if you need to call any methods.
   @ViewChild('player') player!: Player;
   @ViewChild('source') audio!: any;
+  @ViewChild('musicbackground') container!: any;
 
   title: string = 'Music';
   songPlayed: number = 0;
   songs: any = {
-    0: 'assets/tracks/2pac-changes.mp3',
-    1: 'assets/tracks/lp-papercut.mp3',
-    2: 'assets/tracks/lp-intheend.mp3'
+    // 0: 'assets/tracks/2pac-changes.mp3',
+    // 1: 'assets/tracks/lp-papercut.mp3',
+    // 2: 'assets/tracks/lp-intheend.mp3'
+    0: {
+      artist: '2pac',
+      title: 'Changes',
+      songUrl: 'assets/tracks/2pac-changes.mp3',
+      imageUrl: 'assets/imgs/song-background.jpg',
+      album: 'Greatest Hits'
+    },
+    1: {
+      artist: 'Linkin Park',
+      title: 'Papercut',
+      songUrl: 'assets/tracks/lp-papercut.mp3',
+      imageUrl: 'assets/imgs/hey-1.jpg',
+      album: 'Hybrid theory'
+    },
+    2: {
+      artist: 'Linkin Park',
+      title: 'In The End',
+      songUrl: 'assets/tracks/lp-intheend.mp3',
+      imageUrl: 'assets/imgs/hey-2.jpg',
+      album: 'Hybrid theory'
+    }
   }
 
-  constructor(private er: ElementRef) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
@@ -42,7 +64,7 @@ export class MusicComponent implements OnInit {
     await this.player.pause();
     const audioElement = <HTMLSourceElement>this.audio.nativeElement;
     const vmFile = audioElement.parentElement as HTMLAudioElement;
-    vmFile.src = this.songs[this.songPlayed];
+    vmFile.src = this.songs[this.songPlayed].songUrl;
     await this.player.play();
   }
 
@@ -55,7 +77,7 @@ export class MusicComponent implements OnInit {
     await this.player.pause();
     const audioElement = <HTMLSourceElement>this.audio.nativeElement;
     const vmFile = audioElement.parentElement as HTMLAudioElement;
-    vmFile.src = this.songs[this.songPlayed];
+    vmFile.src = this.songs[this.songPlayed].songUrl;
     await this.player.play();
   }
 
